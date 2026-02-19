e7aff83dcc4e416d4b03530b698d03e9d44abc1d
«Maksim <maksim070601@gmail.com>
2026-02-19 20:55:10 +0300

    update

--- teaching/fp-ts-car/car-game.ts
+++ teaching/fp-ts-car/car-game.ts
@@ -1,26 +1,26 @@
-//  import { sequenceS } from 'p-ts/Console';
-import * as Eq from 'fp-ts/Apply'
+import { sequenceS } from 'fp-ts/Apply'
+import * as Eq from 'fp-ts/Eq'
 import * as Console from 'fp-ts/Console'
 import * as IO from 'fp-ts/IO'
 import * as IOE from 'fp-ts/IOEither'
 import * as E from 'fp-ts/Either'
 import * as J from 'fp-ts/Json'
 // import { concatAll } from 'fp-ts/Monoid';
-// import * as NonEmptyArray from 'fp-ts/NonEmptyArray';
+import * as NEA from 'fp-ts/NonEmptyArray'
 import * as RA from 'fp-ts/ReadonlyArray'
-// import * as Ord from 'fp-ts/Ord';
-// import * as R from 'fp-ts/Random';
+import * as Ord from 'fp-ts/Ord'
+import * as R from 'fp-ts/Random'
 import * as RTE from 'fp-ts/ReaderTaskEither'
+import * as TE from 'fp-ts/TaskEither'
 import { flow, pipe } from 'fp-ts/function'
 // import * as N from 'fp-ts/number';
 import * as t from 'io-ts'
-// import * as tt from 'io-ts-types';
+import * as tt from 'io-ts-types'
 import { readFileSync } from 'node:fs'
 import path from 'node:path'
 // import path from 'node:path';
 import readline from 'node:readline'
 import { fileURLToPath } from 'node:url'
-import * as NEA from 'fp-ts/NonEmptyArray'
 
 /* Напишем простую игру - оценщик автомобилей.
  * Игра состоит из 10 раундов. Если в раунде игрок оценивает верно, то получает +1 очко, иначе - ничего.
@@ -32,6 +32,7 @@ import * as NEA from 'fp-ts/NonEmptyArray'
  * - у них более дорогой двигатель(дизель > бензин > электро)
  * - у них меньше пробег(с допустимой разницей в 100 км)
  */
+const MAX_ROUNDS = 10
 
 const CarBrand = {
   BMW: 'BMW',
@@ -47,15 +48,13 @@ const CarEngineBrand = {
 } as const
 type TCarEngineBrand = (typeof CarEngineBrand)[keyof typeof CarEngineBrand]
 
-const MAX_ROUNDS = 10
-
 const Settings = t.type({
   minDistance: t.number,
   maxDistance: t.number,
   minYear: t.number,
   maxYear: t.number,
-  carBrand: t.keyof(CarBrand),
-  engine: t.keyof(CarEngineBrand),
+  carBrand: tt.readonlyNonEmptyArray(t.keyof(CarBrand)),
+  engine: tt.readonlyNonEmptyArray(t.keyof(CarEngineBrand)),
 })
 type TSettings = t.TypeOf<typeof Settings>
 
@@ -79,11 +78,10 @@ const Car = t.type({
 })
 
 type TCar = t.TypeOf<typeof Car>
-// type ReadonlyCars = ReadonlyArray<Car>
 
-type DefaultError<TypeError, ErrorInfo = Error> = {
-  readonly type: TypeError
-  readonly error: ErrorInfo
+type PricedCar = Omit<TCar, 'engineBrand' | 'brand'> & {
+  brandPrice: number
+  engineBrandPrice: number
 }
 
 class ReadFileError extends Error {
@@ -92,16 +90,23 @@ class ReadFileError extends Error {
   }
 }
 
-class ReadFileError extends Error {
+class ParseError extends Error {
   constructor(message: string) {
     super(message)
   }
 }
+class DecodeError extends Error {
+  constructor(message: string) {
+    super(message)
+  }
+}
+
+const Answer = t.union([t.literal(0), t.literal(1), t.literal(2)])
 
 const RoundState = t.type({
   car1: Car,
   car2: Car,
-  answer: t.union([t.literal(1), t.literal(2)]),
+  answer: Answer,
 })
 type TRoundState = t.TypeOf<typeof RoundState>
 
@@ -111,21 +116,124 @@ const readFileSyncIOE = IOE.tryCatch(
   () => readFileSync(pathFile, 'utf-8'),
   (e) => new ReadFileError(String(e)),
 )
-
-const settingsDecode = flow(
-  E.tryCatch(Settings.decode, (e) => new ReadFileError(String(e))),
-  IOE.FromEither,
+const parseJson = flow(
+  J.parse,
+  E.mapLeft((e) => new ParseError(String(e))),
 )
 
-const loadSettings = pipe(readFileSyncIOE)
-const log = (data: any) => Console.log(data())
+const settingsDecode = flow(
+  parseJson,
+  E.flatMap(Settings.decode),
+  E.mapLeft((e) => (e instanceof ParseError ? e : new DecodeError(String(e)))),
+  IOE.fromEither,
+)
+
+const loadSettings = pipe(readFileSyncIOE, IOE.flatMap(settingsDecode))
+
+const generateCar = (setting: TSettings): IO.IO<TCar> =>
+  sequenceS(IO.Apply)({
+    year: R.randomInt(setting.minYear, setting.maxYear),
+    brand: R.randomElem(setting.carBrand),
+    engineBrand: R.randomElem(setting.engine),
+    distance: R.randomInt(setting.minDistance, setting.maxDistance),
+  })
+
+const generateRound = (settings: TSettings) =>
+  pipe(
+    IO.Do,
+    IO.bind('car1', () => generateCar(settings)),
+    IO.bind('car2', () => generateCar(settings)),
+    IO.bind('answer', ({ car1, car2 }) => pipe()),
+
+    IOE.fromIO,
+  )
+
+const generateRounds = (settings: TSettings) =>
+  pipe(
+    NEA.range(0, MAX_ROUNDS),
+    NEA.traverse(IOE.ApplicativeSeq)(() => generateRound(settings)),
+  )
+
+const validateAnswer = (
+  answer: string,
+  resolve: (value: TRoundState['answer']) => void,
+  reject: (reason?: any) => void,
+) =>
+  pipe(
+    answer,
+    Number,
+    Answer.decode,
+    E.fold(
+      (e) => reject(new Error(String(e))),
+      (a) => resolve(a),
+    ),
+  )
+
+const ask =
+  (
+    question: string,
+  ): RTE.ReaderTaskEither<readline.Interface, Error, TRoundState['answer']> =>
+  (rl) =>
+    TE.tryCatch(
+      () =>
+        new Promise<TRoundState['answer']>((resolve, reject) => {
+          rl.question(question, (answer) => validateAnswer(answer, resolve, reject))
+        }),
+      (e) => new Error(String(e)),
+    )
+
+const formatCarInline = (car: TCar): string =>
+  `${car.brand} | ${car.engineBrand} | ${car.year} | ${car.distance} км`
+
+const formatRound = (round: TRoundState, index: number): string => {
+  return `
+  \n 1 - машина 1 дороже, 2 - машина 2 дороже, 0 - машины одинаковые? 
+===============================
+Раунд ${index + 1}
+
+Машина 1
+-------------------------------
+${formatCarInline(round.car1)}
+
+Машина 2
+-------------------------------
+${formatCarInline(round.car2)}
+
+===============================
+`
+}
+
+const startRound = (round: TRoundState, index: number) =>
+  pipe(
+    ask(formatRound(round, index)),
+    RTE.map((answer) => ({ ...round, answer })),
+  )
+
+const runGame = (rounds: TRoundState[]) =>
+  pipe(
+    rounds,
+    RA.mapWithIndex((i, r) => [i, r] as const),
+    RA.traverse(RTE.ApplicativeSeq)(([i, r]) => startRound(r, i)),
+  )
+
+/* Напишем простую игру - оценщик автомобилей.
+ * Игра состоит из 10 раундов. Если в раунде игрок оценивает верно, то получает +1 очко, иначе - ничего.
+ *
+ * Настройки игры читаются из settings.json.
+ * В нашей игре машины дороже когда
+ * - они новее
+ * - у них более дорогая марка(BMW > Audi > Ford)
+ * - у них более дорогой двигатель(дизель > бензин > электро)
+ * - у них меньше пробег(с допустимой разницей в 100 км)
+ */
+
+// const calculateScore = (rounds: TRoundState[]) => pipe()
+
 const run = pipe(
   loadSettings,
-  log,
-  // IOE.flatMap(generateRounds),
-  // IOE.orElse(logs),
-  // RTE.fromIOEither,
-  // RTE.flatMap(runGame),
+  IOE.flatMap(generateRounds),
+  RTE.fromIOEither,
+  RTE.flatMap(runGame),
   // RTE.map(calculateScore),
   // RTE.flatMap(finishGame),
 )
@@ -135,15 +243,15 @@ const run = pipe(
     input: process.stdin,
     output: process.stdout,
   })
-  //const main = run(rl)
-  const result = run()
-  // console.log(result)
+  const main = run(rl)
 
-  // const result = await main()
+  const result = await main()
 
-  // if (E.isLeft(result)) {
-  // console.error(result.left)
-  // }
+  if (E.isLeft(result)) {
+    console.error(result.left)
+  } else {
+    console.log(result.right)
+  }
 
   rl.close()
 })()
